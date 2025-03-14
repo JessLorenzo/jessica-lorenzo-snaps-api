d0a2d9ae543ff9acfe8e051ef0b6960936be4c5f
Jessica Lorenzo <jessicalorenzo32@gmail.com>
2025-03-14 19:42:05 -0400

    create get endpoint for tags

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import photosRouter from "./routes/photos.js";
+import tagsRouter from "./routes/tags.js";
 
 const PORT = process.env.PORT || 8081;
 const app = express();
@@ -14,6 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/photos", photosRouter);
+app.use("/tags", tagsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- routes/tags.js
+++ routes/tags.js
@@ -1,3 +1,12 @@
 import express from "express";
+import fs from "fs";
 
-const path = "../data/tags.json";
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const tags = JSON.parse(fs.readFileSync("./data/tags.json"));
+
+  res.json(tags);
+});
+
+export default router;
